96d2ac74a5847307b80c8d68c8f7134b7938e61c
lizhouxx <42340608+lizhouxx@users.noreply.github.com>
2018-08-17 10:57:02 +0800

    Update HelloWorld.scala

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,12 +1,6 @@
 object HelloWorld {
   def main(args: Array[String]): Unit = {
     println("aaa")
-    println("123")
-    println("bb1")
-    println("123")
-    println("bb1")
-    println("bb1")
-    println("123")
   }
 
 }
